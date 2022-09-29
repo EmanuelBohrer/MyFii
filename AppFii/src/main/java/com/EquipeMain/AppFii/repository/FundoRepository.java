8af021a7bfdb3ded7f87011b274661cac886ecf2
Emanuel Bohrer <100229334+EmanuelBohrer@users.noreply.github.com>
2022-09-29 18:07:23 -0300

    Delete AppFii directory

--- AppFii/src/main/java/com/EquipeMain/AppFii/repository/FundoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.EquipeMain.AppFii.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.EquipeMain.AppFii.models.Fundo;
-
-public interface FundoRepository extends JpaRepository<Fundo, String>  {
-
-}
